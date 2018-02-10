bddefb48936110264c7dba1ed2e69c4f84e76edf
George Lopez-Ayala <swiftcode.design@gmail.com>
2018-02-09 17:44:17 -0800

    'm'

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,51 @@
 import React, { Component} from 'react'
-
+import AppBar from 'material-ui/AppBar'
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import axios from 'axios'
 
 
 class HomePage extends Component {
-
+  constructor(props){
+    super(props);
+    this.state = {
+      cars: []
+    }
+  }
+  componentDidMount(){
+    axios.get('/api/cars').then(cars => {
+      this.setState({cars: cars.data.length ? cars.data : []})
+    })
+  }
 
   render() {
 
     return (
       <div>
-          hello
+          <div >
+            {this.state.cars.map((car, idx) => {
+                const { kb_price, make, model, miles, pics, selling_price, year, description } = car.row_to_json
+                const images = car.row_to_json.pics.map(pic => (
+                  {original: pic.url, thumbnail: pic.url}
+                ))
+                return (
+                  <Card key={idx}>
+                    <CardHeader title={`${make} ${model}`} subtitle={selling_price}/>
+                    <CardMedia overlay={<CardTitle title={`${make} ${model}`} subtitle={year || 'N/A'}/>}>
+                      <img src={images[0].original}/>
+                    </CardMedia>
+                    <CardTitle title={`${make} ${model}`} subtitle={`price: ${selling_price} miles: ${miles}`} />
+                    <CardText>
+                      {description}
+                    </CardText>
+                    <CardActions>
+                      <FlatButton label="action1" />
+                      <FlatButton label="action2" />
+                    </CardActions>
+                  </Card>
+                )
+              })}
+          </div>
       </div>
     )
   }
